decd166659873689dbfb4f5821ce686cb01b2653
Mac <mac@macs-macbook-pro.local>
2018-12-07 16:10:58 +0530

    Done

--- ShareWithWhatsApp/ViewController.swift
+++ ShareWithWhatsApp/ViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var documentIC:UIDocumentInteractionController!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       shareImg()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+func shareImg()
+{
+    let urlWhats = "whatsapp://app"
+    if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
+        if let whatsappURL = URL(string: urlString) {
+            if UIApplication.shared.canOpenURL(whatsappURL) {
+                
+                if let image = UIImage(named: "01") {
+                    if let imageData = UIImageJPEGRepresentation(image, 1.0) {
+                        let tempFile = NSURL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/whatsAppTmp.jpeg")
+                        do {
+                            try imageData.write(to: tempFile!, options: .atomic)
+                            
+                            self.documentIC = UIDocumentInteractionController(url: tempFile!)
+                            self.documentIC.uti = "net.whatsapp.image"
+                            self.documentIC.presentOpenInMenu(from: CGRect.zero, in: self.view, animated: true)
+                        }
+                        catch {
+                            print(error)
+                        }
+                    }
+                }
+                
+            } else {
+                // Cannot open whatsapp
+            }
+        }
+    }
 
+    }
+    func shareVdo()
+    {
+        let urlWhats = "whatsapp://app"
+        if let urlString = urlWhats.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
+            if let whatsappURL = URL(string: urlString) {
+                if UIApplication.shared.canOpenURL(whatsappURL) {
+                    
+                    if let image = UIImage(named: "01") {
+                        if let imageData = UIImageJPEGRepresentation(image, 1.0) {
+                            let tempFile = NSURL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent("Documents/whatsAppTmp.jpeg")
+                            do {
+                                try imageData.write(to: tempFile!, options: .atomic)
+                                
+                                self.documentIC = UIDocumentInteractionController(url: tempFile!)
+                                self.documentIC.uti = "net.whatsapp.image"
+                                self.documentIC.presentOpenInMenu(from: CGRect.zero, in: self.view, animated: true)
+                            }
+                            catch {
+                                print(error)
+                            }
+                        }
+                    }
+                    
+                } else {
+                    // Cannot open whatsapp
+                }
+            }
+        }
+        
+    }
 
 }
 
